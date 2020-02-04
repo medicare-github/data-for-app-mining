1d33642f6ddd56f39c6021aa3481a22344c50c79
medicare <mcareducation@gmail.com>
2020-02-04 20:35:02 +0700

    add title

--- index.php
+++ index.php
@@ -31,46 +31,6 @@
 	include 'includes/header.php'
 	?>
 	<!-- Header section end -->
-	<!-- <section class="hero-section">
-		<div class="hero-slider owl-carousel owl-loaded">
-			<div class="owl-stage-outer">
-				<div class="owl-stage" style="transform: translate3d(-1986px, 0px, 0px); transition: all 0s ease 0s; width: 5958px;">
-					<div class="owl-item animated owl-animated-in fadeIn cloned" style="width: 993px;">
-						<div class="hs-item">
-							<div class="container">
-								<div class="row">
-									<div class="col-lg-6">
-										<div class="hs-text">
-											<h2><span>Data</span> Mining.</h2>
-											<p> merupakan Tool pengumpulan data <span style="color: blueviolet">Nilai Ujian Nasional</span> Mahasiswa Bumigora yang berguna untuk <span class="text-warning">data set Aplikasi Sistem Rekomendasi Pemilihan Jurusan Program Study dengan metode KNN </span>  di <span style="color: blue">universitas Bumigora</span>  </p>
-											<div class="row">
-												<div class="col-md-6 col-sm-6">
-													<a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLScUlqyGXu3yOFVhGYvTNOPALmUQjr2D1PVZyU_wWqyGmXG1zQ/viewform" class="site-btn" style="font-weight: bold"> SMA MIPA</a>
-												</div>
-												<div class="col-md-6 col-sm-6" >
-													<a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSfDg2Yqb1sW8LOHvdplFOVHXKFF-SbjyzvN0j9Bda1a1cinJA/viewform" class="site-btn" style="font-weight: bold">SMA SOS</a>
-												</div>
-												<div class="col-md-11 col-sm-11">
-													<a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLSeCT3uMStsMl4OunO0sy4MElasuNOpkFvT8UciiM1UmNgvtfw/viewform" class="site-btn sb-c2 btn-block"  style="font-weight: bold">SMK Semua Jurusan</a>
-												</div>
-											</div>
-											<a href="#" class="site-btn sb-c2">Start free trial</a>
-										</div>
-									</div>
-									<div class="col-lg-1"></div>
-									<div class="col-lg-6 mt-5">
-										<div class="hr-img">
-											<img src="images/university.png" height="500px" alt="">
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section> -->
 	<section class="how-section spad set-bg" data-setbg="images/how-to-bg.jpg">
 		<div class="container text-white">
 			<div class="row">
@@ -82,6 +42,7 @@
 				</div>
 				<div class="col-md-12">
 					<div class="row text-center">
+						<!-- isi link di href="link" -->
 						<div class="col-md-4 col-sm-12 mb-2">
 							<a target="_blank" href="" class="site-btn" style="font-weight: bold"> SMA Biologi</a>
 						</div>
